69419e364705966cbe175e9615fa9a06c8ebf5d4
Evans Stepanov <evans@stepanov.email>
2017-12-26 19:16:35 -0500

    score implementation

--- logic.js
+++ logic.js
@@ -1,4 +1,41 @@
 'use strict'
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 function centerBall(leftCorner, faceWidth, ballWidth) {
   return leftCorner + faceWidth / 2 - ballWidth / 2
+}
+
+function updateScore(scoreDiv, newScore) {
+  scoreDiv.innerHTML = newScore
+}
+
+function makeOpponent(windowWidth, windowHeight) {
+  let opponentsArray = []
+
+  const NORMAL_HEIGHT = 100
+  const NORMAL_WIDTH = 100
+
+  opponentsArray.push({
+    points: Math.random() * 100,
+    height: NORMAL_HEIGHT,
+    width: NORMAL_WIDTH,
+    xPosition: getRandomInt(0, windowWidth - NORMAL_WIDTH),
+    yPosition: getRandomInt(0, windowHeight - NORMAL_HEIGHT)
+  })
+  return opponentsArray
+}
+
+function drawOpponents(boardContext, opponentsArray) {
+  opponentsArray.forEach(opponent => {
+    console.log('drawing opponent with points', opponent.points)
+    boardContext.fillStyle = '#fff'
+    boardContext.fillRect(
+      opponent.xPosition,
+      opponent.yPosition,
+      opponent.height,
+      opponent.width
+    )
+  })
 }
