4e560247c07a681124695a111a2d708669549e37
Evans Stepanov <evans@stepanov.email>
2017-12-28 18:10:27 -0500

    rough version complete

--- logic.js
+++ logic.js
@@ -12,8 +12,32 @@ function updateScore(scoreDiv, newScore) {
   scoreDiv.innerHTML = parseInt(scoreDiv.innerHTML) + newScore
 }
 
+function endGame(gameOverDiv, scoreDiv, startTime) {
+  gameOverDiv.style.visibility = 'visible'
+  const endTime = new Date().getTime()
+  const gameTime = (endTime - startTime) / 1000
+  gameOverDiv.innerHTML =
+    gameOverDiv.innerHTML +
+    'finished in ' +
+    gameTime +
+    'seconds <br> with a score of ' +
+    scoreDiv.innerHTML
+}
+
 function checkHitDetection(player, opponents, scoreDiv) {
-  opponents.forEach(opponent => {})
+  var newOpponents = []
+  opponents.forEach(opponent => {
+    if (
+      player.XPosition <= opponent.xPosition &&
+      player.YPosition <= opponent.yPosition &&
+      player.XPosition + player.Width >= opponent.xPosition + opponent.width &&
+      player.YPosition + player.Height >= opponent.yPosition + opponent.height
+    ) {
+      updateScore(scoreDiv, opponent.points)
+    } else {
+      newOpponents.push(opponent)
+    }
+  })
   return newOpponents
 }
 
@@ -23,19 +47,21 @@ function makeOpponent(windowWidth, windowHeight) {
   const NORMAL_HEIGHT = 100
   const NORMAL_WIDTH = 100
 
-  opponentsArray.push({
-    points: Math.random() * 100,
-    height: NORMAL_HEIGHT,
-    width: NORMAL_WIDTH,
-    xPosition: getRandomInt(0, windowWidth - NORMAL_WIDTH),
-    yPosition: getRandomInt(0, windowHeight - NORMAL_HEIGHT)
-  })
+  for (var i = 0; i < 2; i++) {
+    opponentsArray.push({
+      points: getRandomInt(1, 2),
+      height: NORMAL_HEIGHT,
+      width: NORMAL_WIDTH,
+      xPosition: getRandomInt(100, windowWidth - NORMAL_WIDTH - 100),
+      yPosition: getRandomInt(100, windowHeight - NORMAL_HEIGHT - 100)
+    })
+  }
+
   return opponentsArray
 }
 
 function drawOpponents(boardContext, opponentsArray) {
   opponentsArray.forEach(opponent => {
-    console.log('drawing opponent with points', opponent.points)
     boardContext.fillStyle = '#fff'
     boardContext.fillRect(
       opponent.xPosition,
